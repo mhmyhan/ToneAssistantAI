61634ae7af231efcc0be04738c42e1f74313ca0e
mhmyhan <max.myhan@gmail.com>
2026-03-16 23:17:00 +0000

    Changed to stereo for Interface, explicit audio driver selection

--- src/live/live_pedalboard.py
+++ src/live/live_pedalboard.py
@@ -4,11 +4,15 @@ import numpy as np
 
 print("Initialising live...")
 
-print(sd.query_devices())
+# use this command (use testing.ipynb) to find your input and output device IDs
+# print(sd.query_devices())
 
-# both majoritively determine latency and quality of audio (adjust to system requirements)
+input_id = 1 # change to your audio input device ID (see testing.ipynb)
+output_id = 11 # change to your audio output device ID (see testing.ipynb)
+
+# these majoritively determine latency and quality of audio (adjust to system requirements)
 sample_rate = 44100 # in Hz     (lower = more latency but less CPU usage)
-block_size = 256 # in samples   (lower = less latency but more CPU usage)
+block_size = 512 # in samples   (lower = less latency but more CPU usage)
 
 # example pedalboard setup that can be controlled using ToneAsst Later
 demoboard = Pedalboard([
@@ -24,18 +28,23 @@ def audio_callback(indata, outdata, frames, time, status):
     if status:
         print(status)
 
+    right_channel = indata[:, 1] #input 2 (right) us used for processing
+
     # Process the audio through the pedalboard
-    audio = indata.T
+    audio = right_channel.reshape(1, -1) # adjust shape to right channel exclusive
 
     processed = demoboard(audio, sample_rate)
 
-    outdata[:] = processed.T
+    out = np.repeat(processed.T, 2, axis=1) # duplicate processed signal into stereo
+
+    outdata[:] = out
 
 with sd.Stream(
     samplerate=sample_rate,
     blocksize=block_size,
-    channels=1,
-    callback=audio_callback):
+    channels=(2, 2), # 2 input & 2 output channels
+    callback=audio_callback,
+    device=(input_id, output_id)):
 
     print("Live pedalboard is running. Ctrl+C to exit.")
 
